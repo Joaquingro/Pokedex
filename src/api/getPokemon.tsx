852568ac86f8c55682e87233c56a957347c0dada
Joaquingro <joaquinsgro@gmail.com>
2023-07-12 20:06:49 -0600

    Add CSS and buttons  in Pokemon, better view

--- src/api/getPokemon.tsx
+++ src/api/getPokemon.tsx
@@ -17,7 +17,7 @@ export const getPokemon = async (name: string) => {
             hp: data.stats[0].base_stat,
             attack: data.stats[1].base_stat,
             deffense: data.stats[2].base_stat,
-            abilities: [data.abilities[0].ability.name, data.abilities[1].ability.name],
+            abilities: [data.abilities[0].ability.name, data.abilities[1]?.ability.name],
             moves: [data.moves[0].move.name, data.moves[1].move.name, data.moves[2].move.name, data.moves[3].move.name, data.moves[4].move.name], 
             id: data.id,
             
--- src/pages/Pokemon/Pokemon.tsx
+++ src/pages/Pokemon/Pokemon.tsx
@@ -2,7 +2,14 @@ import { useParams } from "react-router-dom";
 import style from "../Pokemon/Pokemon.module.css";
 import { useEffect, useState } from "react";
 import { getPokemon } from "../../api/getPokemon";
-export default function Pokemon({Link}: any) {
+
+interface MyComponentProps {
+  Link: React.ComponentType<any>;
+}
+
+ const Pokemon: React.FC<MyComponentProps> = ({Link} ) =>  {
+
+
   const {name} = useParams();
   const [pokemon, setPokemon] = useState<{
     name: string;
@@ -11,48 +18,91 @@ export default function Pokemon({Link}: any) {
     hp: number;
     attack: number;
     deffense: number;
-    abilities: string;
-    moves: string;
+    abilities: Array<[]>;
+    moves: Array<[]>;
     id: number, 
   } | null>(null);
   
   console.log(name);
   console.log(pokemon);
   
-
-useEffect(() => {
-  const onePokemon = async () => {
-    if(name){
-      const pokemonSelected: any = await getPokemon(name.toLocaleLowerCase());
-      setPokemon(pokemonSelected);
-
+  
+  useEffect(() => {
+    const onePokemon = async () => {
+      if(name){
+        const pokemonSelected: any = await getPokemon(name.toLocaleLowerCase());
+        setPokemon(pokemonSelected);
+        
+      }
     }
-  }
-  onePokemon();
-}, [name])
-
+    onePokemon();
+  }, [name])
+  const maxHP = 150;
   return (
     
     <div className={style.container}>
+    
       {pokemon && (
       <div className={style.pokemon}>
+      <div className={style.buttons}>
+       <div className={style.shiny}> 
+        <button type="button" className={`btn btn-danger ${style.buttonshiny}`}>SHINY</button>
+       </div>
       <img src={pokemon.img}/>
+        <Link to = "/pokemons">
+              <button type="button" className="btn btn-danger">Regresar</button>
+      </Link>
+      </div>
       <div className={style.info}>
-        <p>Nombre: {name}</p>
-        <p>Id: {pokemon.id}</p>
-        <p>Ataque: {pokemon.attack}</p>
-        <p>Defensa: {pokemon.deffense}</p>
-        <p>Movimientos:</p><p>{pokemon.moves}</p>
-        <p>Habilidades:</p><p>{pokemon.abilities}</p>
-        <p></p>
+        <div className={style.info1}>
+          <p>Name: {name}</p>
+          <p>Id: {pokemon.id}</p>
+        </div>
+        <div className={style.info2}>
+            <p className={style.title}>Statistics</p>
+
+            <p>HP:</p>
+          <div className={`progress ${style.progress}`} role="progressbar" aria-label="Success example" aria-valuenow={pokemon.hp} aria-valuemin={0} aria-valuemax={maxHP}>
+            <div className="progress-bar bg-danger" style={{width: `${pokemon.hp / maxHP * 100}%`}}>{pokemon.hp}</div>
+          </div>
+
+            <p>Attack:</p>
+            <div className={`progress ${style.progress}`} role="progressbar" aria-label="Info example" aria-valuenow={pokemon.attack} aria-valuemin={0} aria-valuemax={maxHP}>
+            <div className={`progress-bar bg-warning text-white`} style={{width: `${pokemon.attack / maxHP * 100}%`}}>{pokemon.attack}</div>
+          </div>
+          
+            <p>Defense:</p>
+            <div className={`progress ${style.progress}`} role="progressbar" aria-label="Success example" aria-valuenow={pokemon.deffense} aria-valuemin={0} aria-valuemax={maxHP}>
+            <div className="progress-bar bg-info text-white" style={{width: `${pokemon.deffense / maxHP * 100}%`}}>{pokemon.deffense}</div>
+          </div>
+        </div>
+        <div className={style.info3}>
+          <div>
+            <span className={style.title}>Moves:</span> 
+          <div className={style.moves}>
+            <p>{pokemon.moves[0]}</p>
+            <p>{pokemon.moves[1]}</p>
+            <p>{pokemon.moves[2]}</p>
+            <p>{pokemon.moves[3]}</p>
+
+            </div>
+            </div>
+          <div>
+          <span className={style.title}>Abilities:</span>
+          <div className={style.moves}>
+            <p>{pokemon.abilities[0]}</p>
+            <p>{pokemon.abilities[1] ? pokemon.abilities[1] : "---"}</p>
+          </div>
+          </div>
+        </div>
       </div>
       
       </div>
       )} 
-      <Link to = "/pokemons">
-              <button type="button" className="btn btn-outline-danger">Regresar</button>
-      </Link>
+      
 
     </div>
   )
 }
+
+export default Pokemon;
